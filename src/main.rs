e57ead70a55f6123e6c85fda8d052158069309b7
Teemu Pätsi <teemu@paddy.fi>
2022-09-06 15:22:12 +0300

    Fix syntax for test file + add debug print

--- src/main.rs
+++ src/main.rs
@@ -9,4 +9,5 @@ fn main() {
   let code: String = fs::read_to_string("test.pog").expect("Failed to read the file");
   let mut parser: Parser = Parser::init(code.as_str());
   let tokens: Vec<Token> = parser.parse();
+  dbg!(tokens);
 }
