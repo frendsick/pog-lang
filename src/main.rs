0be5d6349d40ee296d827a82425666e33b8bdb02
CyberPaddy <teemu@paddy.fi>
2022-09-10 15:44:47 +0300

    Commented required steps for compilation

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,22 @@ use defs::Program;
 
 use lexer::Parser;
 fn main() {
-  let code: String = fs::read_to_string("test.pog").expect("Failed to read the file");
+  // TODO: Parse command line arguments
+
+  // Read code files
+  // TODO: Enable including code from multiple files
+  let code: String = fs::read_to_string("test.pog")
+    .expect("Failed to read the file");
+
+  // Parse code to Tokens
   let mut parser: Parser = Parser::init(code.as_str());
   let tokens: Vec<&str> = parser.parse();
 
+  // Generate abstract syntax tree (AST)
   let program: Program = generate_ast(&tokens);
   dbg!(&program);
+
+  // TODO: Type check Program
+  // TODO: Generate assembly code
+  // TODO: Compile executable
 }
